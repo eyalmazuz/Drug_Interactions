8858494fddf38ee3a99054fd7d0be6afbaa39f0a
eyalmazuz <mazuze@post.bgu.ac.il>
2021-04-12 20:40:11 +0300

    removed webhook

--- drug_interactions/utils.py
+++ drug_interactions/utils.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-webhook_url = 'https://discord.com/api/webhooks/791261360558178324/KgcGEmNWPkM227ZtwWnmEERz0n37Fb642WOgpEWfu5BUan1WhJAVqg95ombetfy6M37y'
+webhook_url = ''
 
 def send_message(text: str):
     headers = {'Content-Type': 'application/json'}
