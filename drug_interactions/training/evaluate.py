switch wrong average precision metric to MAP@K and finished the tta code

drug_interactions/training/evaluate.py
@@ -50,7 +50,6 @@ def predict_tta(model, test_dataset, save: bool=True, **kwargs):
     """
     dataset_type = kwargs.pop('dataset_type')
     path = kwargs.pop('save_path')
-    name = kwargs.pop('name')
     print('Building test dataset.')
 
     drugs_a, drugs_b, predictions, labels = [], [], [], []
@@ -68,19 +67,18 @@ def predict_tta(model, test_dataset, save: bool=True, **kwargs):
             prediction = [np.average(prediction, weights=tta_weights)]
         else:
             prediction = [pred[0] for pred in preds.numpy().tolist()]
-        # print(len(predictions))
+
         predictions += prediction
         labels += labels_batch.tolist()[0]
     send_message(f'{dataset_type} Finished test set')
     print(len(drugs_a), len(drugs_b), len(labels), len(predictions))
     df = pd.DataFrame({'Drug1_ID': drugs_a, 'Drug2_ID': drugs_b, 'label': labels, 'prediction': predictions})
     if save:
-        df.to_csv(f'{path}/{dataset_type}_{name}.csv', index=False)
+        df.to_csv(f'{path}/{dataset_type}.csv', index=False)
 
-        calc_metrics(path, f'{dataset_type}_{name}')
+        calc_metrics(path, dataset_type)
 
     send_message(f'Finished {dataset_type}')
-    # print(confusion_matrix(y_true=df.label.tolist(), y_pred=df['class'].tolist()))
 
 @tf.function()
 def _test_step(model, inputs: tf.Tensor, **kwargs) -> None:

drug_interactions/utils/calc_metrics.py
@@ -22,7 +22,21 @@ def calc_mrr(df):
  
     return mrr
 
-def calc_average_precision_k(df, k):
+def average_precision_at_k(k, class_correct):
+    # return average precision at k.
+    # more examples: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    # and: https://www.kaggle.com/c/avito-prohibited-content#evaluation
+    # class_correct is a list with the binary correct label ordered by confidence level.
+    score = 0.0
+    hits = 0.0
+    for i in range(k):
+        if class_correct[i] == 1:
+            hits += 1.0
+        score += hits / (i + 1.0)
+    score /= k
+    return score
+
+def calc_mean_average_precision_k(df, k):
     grouped_test = df.groupby('Drug1_ID')
     average_precision = 0.0
     for (_, smile_group) in tqdm(grouped_test, leave=False):
@@ -30,11 +44,12 @@ def calc_average_precision_k(df, k):
         try:
             y_true = np.array(smile_group.label.tolist())
             y_pred = np.array(smile_group['class'].tolist())
-            average_precision += precision_score(y_true=y_true[:k], y_pred=y_pred[:k], zero_division=0)
+            correct_preds = np.logical_and(y_true, y_pred).astype(np.int32)
+            average_precision += average_precision_at_k(k, correct_preds)
         except Exception:
             pass
     average_precision = round(average_precision / df["Drug1_ID"].nunique(), 4)
-    print(f'Average Precision@{k} {average_precision}', end=' ')
+    print(f'Mean Average Precision@{k} {average_precision}', end=' ')
 
     return average_precision
 
@@ -70,7 +85,7 @@ def calc_metrics(path: str, dataset_type: str, df: pd.DataFrame=None) -> None:
     df['class'] = df.prediction.progress_apply(lambda x: 1 if x > 0.5 else 0)
 
     for k in tqdm(top_k):
-        results[f"Average Precision@{k}"] = calc_average_precision_k(df, k)
+        results[f"Mean Average Precision@{k}"] = calc_mean_average_precision_k(df, k)
 
     print()
 

ensemble.py
@@ -1,16 +1,16 @@
 import os 
 
-import numpy as np
 import pandas as pd
 pd.set_option('display.max_columns', 10)
-from sklearn.metrics import log_loss, roc_auc_score, precision_score, precision_recall_curve, auc
-from tqdm import tqdm, trange
+from tqdm import tqdm
 tqdm.pandas()
+
+from drug_interactions.utils.calc_metrics import calc_metrics
 
 test_df = pd.read_csv('./data/csvs/data/test_all_chemprop_similar.csv')
 test_df = test_df.sort_values(by=['Drug1_ID', 'Drug2_ID'], ascending=True).reset_index(drop=True)
 
-path = './data/csvs/results/All_Data/AllChempropSimilar'
+path = './data/csvs/results/All_Data/AllEnsemble'
 
 models = []
 for model in os.listdir(path):
@@ -28,63 +28,8 @@ print(test_df.head())
 
 print(models)
 df = test_df.copy()
-df['mean'] = df[models].mean(axis=1)
+df['prediction'] = df[models].mean(axis=1)
 results = {}
-results['dataset_type'] = 'ENSEMBLE'
+dataset_type = 'ENSEMBLE'
 
-print(df.isna().sum())
-label, prediction = df.label.tolist(), df['mean'].tolist()
-bce = round(log_loss(y_true=label, y_pred=prediction), 4)
-roc_auc = round(roc_auc_score(y_true=label, y_score=prediction), 4)
-precision, recall, _ = precision_recall_curve(y_true=label, probas_pred=prediction)
-pr_auc = round(auc(recall, precision), 4)
-
-results['Log Loss'] = bce
-results['AUC'] = roc_auc
-results['PR AUC'] = pr_auc
-
-print(f'Test BCE: {bce}')
-print(f'Test AUC: {roc_auc}')
-print(f'Test PR-AUC: {pr_auc}')
-
-top_k = list(range(1, 6)) + [10, 20, 50, 100, 200]
-
-df = df.sort_values('mean', ascending=False)
-df['class'] = df['mean'].progress_apply(lambda x: 1 if x > 0.5 else 0)
-
-grouped_test = df.groupby('Drug1_ID')
-
-for k in tqdm(top_k):
-    average_precision = 0.0
-    for (_, smile_group) in tqdm(grouped_test, leave=False):
-        smile_group = smile_group.sort_values('mean', ascending=False)
-        try:
-            y_true = np.array(smile_group.label.tolist())
-            y_pred = np.array(smile_group['class'].tolist())
-            average_precision += precision_score(y_true=y_true[:k], y_pred=y_pred[:k], zero_division=0)
-        except Exception:
-            pass
-    average_precision = round(average_precision / df["Drug1_ID"].nunique(), 4)
-    print(f'Average Precision@{k} {average_precision}', end=' ')
-    results[f"Average Precision@{k}"] = average_precision
-
-print()
-
-mrr = []
-for (_, dg) in tqdm(grouped_test, leave=False):
-    dg['binary'] = dg['mean'].apply(lambda x: 1 if x >= 0.5 else 0)
-    dg = dg.sort_values('mean', ascending=False)
-    dg.reset_index(drop=True, inplace=True)
-    first_hit = dg[(dg['label'] == 1) & (dg['binary'] == 1)]
-    if first_hit.shape[0] == 0:
-        mrr.append(0)
-    else:
-        mrr.append(1 / (first_hit.index[0] + 1))
-
-mrr = round(np.array(mrr).mean(), 4)
-print(f'Test MRR: {mrr}')
-results["MRR"] = mrr
-
-metrics = pd.read_csv(f'{path}/Metrics.csv')
-metrics = metrics.append(pd.Series(results), ignore_index=True)
-metrics.to_csv(f'{path}/Metrics.csv', index=False)
+calc_metrics(dataset_type=dataset_type, path=path, df=df)