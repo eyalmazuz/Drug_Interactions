916f51c1f4ce5cc40349eafc637cd9b36b6240b3
Eyal Mazuz <mazuze@post.bgu.ac.il>
2021-10-16 15:00:45 +0300

    Update utils.py

--- drug_interactions/utils/utils.py
+++ drug_interactions/utils/utils.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-webhook_url = 'https://discord.com/api/webhooks/831222360102142052/oiWQffWWECZCFDIDylr9hZuAV909DXifDX0WKZWTinq6tS8LDamfD2PjsuDekIkUIf6y'
+webhook_url = ''
 
 def send_message(text: str):
     headers = {'Content-Type': 'application/json'}
